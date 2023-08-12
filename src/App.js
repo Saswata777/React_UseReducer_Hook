99548645985acedf52dc6542aee52e656bd585de
Saswata <saswatamaitra3.14@gmail.com>
2023-08-12 21:56:43 +0530

    Adding file for the first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from 'react' 
+const initialState = 0;
+
+const reducer = (state, action)=>{
+    switch (action) {
+      case "Increment":
+        return  state + 1
+      
+      case "Decrement":
+        return  state - 1
+      default:
+            return state
+    }
+
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <div className="header">Application Of UseReducer Hook</div>
+          <div className="body">
+              <div className='box'>
+                  <div className='countDisplay'>Count: {count}</div>
+                  <div className="buttonGroup">
+                      <button onClick={()=>dispatch("Increment")}> Increment</button>
+                      <button onClick={()=>dispatch("Decrement")}> Decrement</button>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
